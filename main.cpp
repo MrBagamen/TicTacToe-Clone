92109d94ea5eca1a45e4fb9b5e8b49b84b993b06
Mika Attila <radiantstatue@gmail.com>
2013-09-24 19:28:37 +0200

    main.cpp: Remove unused includes

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
-#include "Sprite.hpp"
-#include "util.hpp"
 #include "Game.hpp"
 #include "Menu.hpp"
 #include "driver.hpp"
-
-#include <iostream>
-#include <string>
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-
-using namespace std;
 
 int main()
 {
